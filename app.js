4b54f195f147b2e598a2eaa9ff40777ed3201fe8
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-06-15 18:24:37 -0400

    take img out of div

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
 });
 
 function showGif() {
-  console.log('showing');
+  //TODO push gifs to arr and randomize placement
+  // var imgArr = [];
   var artist = 'prince';
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + artist + "&api_key=dc6zaTOxFJmzC&limit=6";
   //call
@@ -14,27 +15,48 @@ function showGif() {
     console.log(results.length);
     for (let j = 0; j < 2; j++) {
       for (var i = 0; i < results.length; i++) {
-        imgUrl = results[i].images.fixed_width.url;
+        imgUrl = results[i].images.fixed_height.url;
         imgUrlstill = 'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=3864424';
 
-        var container = $('div');
-        container.attr('class', 'container');
-        var row = $('row');
-        row.attr('class', 'row');
-        var princeDiv = $('div');
-        princeDiv.attr('class', 'col-xs-4 col-sm-3 col-md-3 col-lg-2.5 img thumbnail');
+        // var container = $('div');
+        // container.attr('class', 'container');
+        // var row = $('row');
+        // row.attr('class', 'row');
+        // var princeDiv = $('div');
         var princeImg = $('<img>');
+        princeImg.attr('class', 'col-xs-4 col-sm-3 col-md-3 col-lg-2.5 img thumbnail');
         princeImg.attr('src', imgUrlstill).attr('class', 'card img-fluid img-thumbnail').attr('data-state', 'still').attr('data-still', imgUrlstill).attr('data-animate', imgUrl);
 
-        princeDiv.append(princeImg);
-        row.append(princeDiv);
-        $('game').append(row);
+        $('.container').append(princeImg);
+        // row.append(princeDiv);
+        // $('container').append(row);
 
+        // imgArr.push(princeDiv);
       }
     }
+    // console.log(imgArr);
   });
+  var card1, card2;
   $(document).on("click", ".card", function() {
+    console.log('card1: ',card1,'card2: ',card2)
 
+    if(card1===undefined){
+      card1= $(this).attr('data-animate');
+      console.log('setting card1: ',card1);
+    }else if(card1 != undefined && card2=== undefined){
+      card2 = $(this).attr('data-animate');
+      console.log('card1: ',card1,' ','setting card2: ',card2)
+      if( card1 === card2){
+        console.log('match!');
+        card1=undefined;
+        card2=undefined;
+      }else{
+        card1=undefined;
+        card2=undefined;
+      }
+    }
+
+//TODO use set timeout to set state to animate for 3 seconds;
     var state = $(this).attr('data-state');
 
     if ($(this).attr('data-state') == 'still') {
@@ -46,10 +68,3 @@ function showGif() {
     }
   });
 };
-
-$(document).on("mouseout",".card", function(){
-
-    $(this).attr('src', $(this).attr('data-still'));
-    $(this).attr('data-state', 'still');
-
-})
